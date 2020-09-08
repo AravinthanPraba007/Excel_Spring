7e604e0d21847f28cd94551b0736a75aefc96ed0
Aravinthan Praba <799532>
2020-09-09 00:29:40 +0530

    added the from to model class

---
+++ src/main/java/com/example/excel/model/FromTo.java
@@ -0,0 +1,29 @@
+package com.example.excel.model;
+
+public class FromTo {
+
+	int from;
+	int to;
+	public int getFrom() {
+		return from;
+	}
+	public void setFrom(int from) {
+		this.from = from;
+	}
+	public int getTo() {
+		return to;
+	}
+	public void setTo(int to) {
+		this.to = to;
+	}
+	public FromTo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "FromTo [from=" + from + ", to=" + to + "]";
+	}
+	
+	
+}
